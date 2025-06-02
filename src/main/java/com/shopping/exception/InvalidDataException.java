c6c976e68f1b105b1afe88f7a288311bf2f70ea3
Rohit Sonwane <89028174+rohitsonwane652@users.noreply.github.com>
2025-06-02 16:05:45 +0530

    Create InvalidDataException.java

---
+++ src/main/java/com/shopping/exception/InvalidDataException.java
@@ -0,0 +1,12 @@
+package com.shopping.exception;
+
+import java.io.Serializable;
+
+public class InvalidDataException extends Exception implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidDataException(String message){
+        super(message);
+    }
+}
