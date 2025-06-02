7216e6b563faee46873b7095da3e8d2bc241e7b5
Rohit Sonwane <89028174+rohitsonwane652@users.noreply.github.com>
2025-06-02 16:08:57 +0530

    Update LogMessage.java

--- src/main/java/com/shopping/utils/LogMessage.java
+++ src/main/java/com/shopping/utils/LogMessage.java
@@ -1,21 +1,32 @@
 package com.shopping.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Slf4j
 public class LogMessage {
 
-    @Autowired
-    static Logger logger = LoggerFactory.getLogger(LogMessage.class);
-
     public static void infoLog(String message){
-        logger.info("Shopping app info message:" + message);
+        log.info("Shopping app info message -->" + message);
     }
 
     public static void errorLog(String message){
-        logger.error("Shopping app error message:" + message);
+        log.error("Shopping app error message -->" + message);
     }
+
+    public static void startLog(String className, String methodName) {
+        log.info("******* " + className + " ******* " + methodName + " ******* START *******");
+    }
+
+    public static void endLog(String className, String methodName) {
+        log.info("******* " + className + " ******* " + methodName + " ******* END *******");
+    }
+
+    public static void logStackTrace(String exception, Exception e) {
+        log.error("Info Message :: Exception Object -->" + exception + " :: Detailed Info ", e);
+    }
+
 }
