ed68b4b4cce1c335b9cd9d4e37761c950eb1eae3
Rohit Sonwane <89028174+rohitsonwane652@users.noreply.github.com>
2025-06-02 16:07:44 +0530

    Update RewardService.java

--- src/main/java/com/shopping/service/RewardService.java
+++ src/main/java/com/shopping/service/RewardService.java
@@ -2,6 +2,9 @@ package com.shopping.service;
 
 import com.shopping.model.RewardResponse;
 
+import java.util.List;
+
 public interface RewardService {
-    RewardResponse calculateRewardForUser(Long customerId);
+    RewardResponse calculateRewardForUser(Long customerId) throws Exception;
+    List<RewardResponse> calculateRewardForAllCustomers();
 }
